5ec523929ad24b3f23ad51e95ca99e4350011db7
mateusbrito93 <itzmts@gmail.com>
2025-05-26 22:37:11 -0300

    update footer

--- includes/footer.php
+++ includes/footer.php
@@ -9,7 +9,8 @@
     </div>
 </footer>
 
-<script src="js/script-index.js"></script> <?php if (basename($_SERVER['PHP_SELF']) == 'index.php'): ?>
+<?php if (basename($_SERVER['PHP_SELF']) == 'index.php'): ?>
+    <script src="js/script-index.js"></script>
     <script src="js/lang-index.js"></script>
 <?php elseif (basename($_SERVER['PHP_SELF']) == 'tecido.php'): ?>
     <script src="js/lang-tecido.js"></script>
